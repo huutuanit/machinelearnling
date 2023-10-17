39aac55e2680c444b13ed556f0ff922c36037910
tuanch <huutuanit852000@gmail.com>
2023-10-17 12:58:04 +0700

    add tro ly ao v1.0

---
+++ troly_ao.py
@@ -0,0 +1,46 @@
+import speech_recognition as sr 
+import pyttsx3 as ptt
+from datetime import datetime
+
+robot_ear = sr.Recognizer() 
+robot_brain = "I'm robot"
+you = "Hello"
+robot_mounth = ptt.init()
+
+def _listen():
+    with sr.Microphone() as mic:
+        print("Robot: I'm listening.")
+        try:
+            audio = robot_ear.listen(mic)
+            you = robot_ear.recognize_google(audio)
+        except:
+            you = "" 
+        print("You: " + you)
+
+def _thingking():
+    print("Robot: ...")
+    if you == "":
+        robot_brain = "I can't hear you, try again"
+    elif "hello" in you:
+        robot_brain = "Hello Tuan" 
+    elif "today" in you: 
+        robot_brain = "today is " + datetime.today().strftime('%Y-%m-%d') 
+    elif "time" in you: 
+        robot_brain = "the time is " + datetime.today().strftime('%H:%M') 
+    elif "bye" in you:
+        robot_brain = "Good bye"
+        _speech()
+        exit()
+    else:
+        robot_brain = "I cannot understand what you say, Please try again."
+    print("Robot: " + robot_brain)
+
+def _speech(): 
+    robot_mounth.say(robot_brain)
+    robot_mounth.runAndWait()
+
+# Main process
+while True:
+    _listen()
+    _thingking()
+    _speech()
